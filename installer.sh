7e10fb6917b2ea4074ee3e60589bd88400832b52
codyowl <codyowl@gmail.com>
2020-07-05 20:11:25 +0530

    Installer.sh file added for new box setup :rocket:

---
+++ installer.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+install_gcloud () {
+	echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+	curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+	sudo apt-get update && sudo apt-get install google-cloud-sdk
+}
+
+install_aws () {
+	curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+	unzip awscli-bundle.zip
+	sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+}
+
+install_azure () {
+	sudo apt-get update
+	sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+	curl -sL https://packages.microsoft.com/keys/microsoft.asc |
+    gpg --dearmor |
+    sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+    AZ_REPO=$(lsb_release -cs)
+	echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+
+    sudo apt-get update
+	sudo apt-get install azure-cli
+}
+
+install_gcloud
+install_aws
+install_azure
