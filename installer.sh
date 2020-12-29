f2b51652b19262b5888e81bb8fb6c3b7dbc33c95
codyowl <codyowl@gmail.com>
2020-12-29 21:24:00 +0530

    kubernetes added to installer shell script

--- installer.sh
+++ installer.sh
@@ -26,6 +26,16 @@ install_azure () {
 	sudo apt-get install azure-cli
 }
 
+install_kubernetes(){
+	if kubectl version | grep Client
+	then
+		echo "Kubernetes is already installed"
+	else
+		curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl";
+		chmod +x ./kubectl;
+		sudo mv ./kubectl /usr/local/bin/kubectl
+	
+}
 
 install_gcloud
 install_aws
