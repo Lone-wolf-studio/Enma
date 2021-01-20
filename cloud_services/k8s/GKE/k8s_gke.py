b36ed22e880f013be34e810dea6e5ce07da3805c
codyowl <codyowl@gmail.com>
2021-01-20 22:55:37 +0530

    k8s created :heavy_plus_sign: GKE initialized :baby_bottle:

---
+++ cloud_services/k8s/GKE/k8s_gke.py
@@ -0,0 +1,9 @@
+class GKE:
+    def __init__(self, cluster_name, namespace, additional_zones, cluster_version):
+        self.cluster_name = cluster_name
+        self.namespace = namespace
+        self.additional_zones = additional_zones
+        self.cluters_version = cluster_version
+
+
+
